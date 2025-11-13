0410dffe3a4d215ad239fdee4a0a126b4d8523a8
Jaswanth <jaswanth@10xr.co>
2025-11-13 13:07:21 +0530

    adding supabase declarative configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,16 @@
 import "styles/tailwind.css"
+import { NavBar } from "./components/navBar"
+import { ThemeProvider } from "./components/theme-provider"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <ThemeProvider>
+          <NavBar />
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
